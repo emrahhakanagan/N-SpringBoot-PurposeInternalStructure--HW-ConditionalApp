10ef9a9e4b03b0b1c5b941b96cdbdd0dae036aa5
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-04-06 15:49:08 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## --- NETOLOGY HOMEWORK ---
+### SPRING BOOT, DEPLOYMENT AND INFRASTRUCTURE --> Spring Boot: Purpose, Internal Structure
+
+
+#### --> Task 1 --> Conditional App
+
---
+++ src/main/java/com/agan/springbootpurposeinternalstructurehwconditionalapp/SpringBootPurposeInternalStructureHwConditionalAppApplication.java
@@ -0,0 +1,13 @@
+package com.agan.springbootpurposeinternalstructurehwconditionalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPurposeInternalStructureHwConditionalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPurposeInternalStructureHwConditionalAppApplication.class, args);
+    }
+
+}
