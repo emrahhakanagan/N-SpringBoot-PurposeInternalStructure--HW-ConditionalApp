48a289dc98d6a0d0291ff4180f74be320eef82b0
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-04-20 00:56:19 +0300

    docker-test added

--- src/test/java/com/agan/conditionalapp/ConditionalAppApplicationTests.java
+++ src/test/java/com/agan/conditionalapp/ConditionalAppApplicationTests.java
@@ -1,13 +1,55 @@
 package com.agan.conditionalapp;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-class ConditionalAppApplicationTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    @Test
-    void contextLoads() {
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ConditionalAppApplicationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    public static GenericContainer<?> devapp = new GenericContainer<>("devapp:latest")
+            .withExposedPorts(8080);
+
+    public static GenericContainer<?> prodapp = new GenericContainer<>("prodapp:latest")
+            .withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+        devapp.start();
+        prodapp.start();
     }
 
+    @Test
+    public void testDevProfile() {
+        String response = restTemplate.getForObject("http://localhost:" + devapp.getMappedPort(8080)
+                                                    + "/profile", String.class);
+        assertEquals("Current profile is dev", response);
+    }
+
+    @Test
+    public void testProdProfile() {
+        String response = restTemplate.getForObject("http://localhost:" + prodapp.getMappedPort(8081)
+                                                    + "/profile", String.class);
+        assertEquals("Current profile is production", response);
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        devapp.stop();
+        prodapp.stop();
+    }
 }
+
